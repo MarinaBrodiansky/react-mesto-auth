35d355fc35d5a1fb4d7a3d165bcc98ce2ae135d6
Marina Brodiasky <marina.brodianskaya@yandex.ru>
2023-05-26 15:02:30 +0300

    правки ревью

--- src/index.js
+++ src/index.js
@@ -8,7 +8,8 @@ const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter 
+    basename="mesto-react-auth">
       <App />
     </BrowserRouter>
   </React.StrictMode>
